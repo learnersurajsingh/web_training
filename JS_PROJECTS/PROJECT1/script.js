5a2ce343f1851a5317c1fdb9f43b8bacfd9cc83a
Suraj <learnersurajsingh@gmail.com>
2025-04-11 01:50:22 +0530

    project 2 done

--- JS_PROJECTS/PROJECT1/script.js
+++ JS_PROJECTS/PROJECT1/script.js
@@ -2,19 +2,14 @@ const buttons = document.querySelectorAll('.button')
 const bdy = document.querySelector('body')
 
 buttons.forEach((btn)=>{
-    btn.addEventListener('click', (e)=>{
-        if(e.target.id === 'grey'){
-            bdy.style.backgroundColor = e.target.id
-        }                
-        if(e.target.id === 'white'){
-            bdy.style.backgroundColor = e.target.id
-        }                
-        if(e.target.id === 'yellow'){
-            bdy.style.backgroundColor = e.target.id
-        }                
-        if(e.target.id === 'blue'){
-            bdy.style.backgroundColor = e.target.id
-        }                
+    btn.addEventListener('click',(e)=>{
+        if(e.target.id==='grey')
+            bdy.style.backgroundColor=e.target.id
+        if(e.target.id==='white')
+            bdy.style.backgroundColor=e.target.id
+        if(e.target.id==='blue')
+            bdy.style.backgroundColor=e.target.id
+        if(e.target.id==='yellow')
+            bdy.style.backgroundColor=e.target.id
     })
 })
-
